d805926a068149cc155e293307f4aa35effaaaf4
むずでょ <muzudho1@gmail.com>
2021-09-17 21:59:25 +0900

    きふわらべ色のサンプル

---
+++ WpfWindowResourcesPractice/WpfWindowResourcesPractice/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfWindowResourcesPractice
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
